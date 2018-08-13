b427bc97dbeb95a28c864c023f4218c5de25cc94
zhouzhichao <342412187@qq.com>
2018-08-13 18:08:55 +0800

    '识物页面静态部分完成100%'

--- src/pages/Shiwu/ShiwuBanner/ShiwuBanner.vue
+++ src/pages/Shiwu/ShiwuBanner/ShiwuBanner.vue
@@ -7,6 +7,11 @@
                v-for="item in homeData.focusList" :key="item.id">
             <a href="javascript:;">
               <img :src="item.picUrl" alt="image">
+              <div class="content">
+                <div class="subTitle">严选推荐</div>
+                <div class="title">严选按摩神器大公开</div>
+                <div class="desc">揭开每一款的秘技绝学</div>
+              </div>
             </a>
           </div>
         </div>
@@ -52,7 +57,50 @@
       display: block
       padding: 0 .1333rem
       background-color: #fff
-
+      .content
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: 0
+        margin: auto
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 6.21333rem;
+        height: 2.64rem;
+        background-color: rgba(255,255,255,.9)
+        .subTitle
+          position: relative
+          font-size: .26667rem
+          color: #7f7f7f
+          line-height: .4rem
+          padding: 0 .10667rem
+          &::before, &::after
+            content: ' '
+            position: absolute
+            top: 0
+            bottom: 0
+            margin: auto
+            width: .32rem
+            height: 1px
+            background-color: #999
+          &::before
+            left: -.32rem
+          &::after
+            right: -.32rem
+        .title
+          font-size: .42667rem
+          line-height: .64rem
+          color: #333
+          margin-top: .21333rem
+          font-weight: 700
+        .desc
+          font-size: .32rem
+          color: #333
+          line-height: .48rem
+          margin-top: .05333rem
   .carousel
     position: relative
     background-color: #fff
